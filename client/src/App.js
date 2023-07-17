27e8161338d0efc867115ad64c394bcfd6e796e6
Dmytro Hrinchenko <demonchikk1@gmail.com>
2023-07-18 00:13:15 +0300

    clean app from unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import {Home, Contact, Basket, About, NotFound} from './pages'
- 
+import { Route, Routes } from "react-router-dom";
+import { Home, Contact, Basket, About, NotFound } from "./pages";
+
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/basket' element={<Basket />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='*' element={<NotFound />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
