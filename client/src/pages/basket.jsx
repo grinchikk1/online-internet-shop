7beda185907f247a4f2fe40ea2653877562d5407
Bohdan Kravets <kravets12082002@gmail.com>
2023-07-17 23:27:24 +0300

    add routers

---
+++ client/src/pages/basket.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Basket() {
+  return (
+    <h1>Basket page</h1>
+  )
+}
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export * from './about';
+export * from './basket';
+export * from './contact';
+export * from './home';
+export * from './not-found';
