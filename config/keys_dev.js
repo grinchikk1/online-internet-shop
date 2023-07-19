26457bf9983bc7b211c0a815a365ee123b871605
Dmytro Hrinchenko <demonchikk1@gmail.com>
2023-07-19 15:52:43 +0300

    fix key-dev

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mnogoURI:
+  mongoURI:
     "mongodb+srv://admin:l6JcflyhXpZxvmNB@cluster0.oodqwqd.mongodb.net/?retryWrites=true&w=majority",
   secretOrKey: "random very very secret string",
 };
