b46b396cbd0a567cb497bf3a5fa6afbfdd6fece0
Bohdan Kravets <kravets12082002@gmail.com>
2023-07-24 23:17:29 +0300

    add notfound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import About from "./pages/About/About";
+import Shop from "./pages/Shop/Shop";
 import Cart from "./pages/Cart/Cart";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-
 import "./styles/App.scss";
 
 function App() {
@@ -16,7 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
