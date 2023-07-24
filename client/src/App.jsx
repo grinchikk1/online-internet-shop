e888ea85392090d084000048acf60a170704094c
Bohdan Kravets <kravets12082002@gmail.com>
2023-07-24 23:17:29 +0300

    add notfound page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Cart from "./pages/Cart/Cart";
 import Contact from "./pages/Contact/Contact";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
-
 import "./styles/App.scss";
 
 function App() {
