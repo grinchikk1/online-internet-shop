18b83984f9c309479d09cb61991cb215e444a792
yaGodya <krispygeorg@gmail.com>
2023-07-26 18:13:40 +0300

    Temporary commit to save local changes

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-import { Box, FormControl, TextField, Typography } from "@mui/material";
-import { styles } from "./InputStyle";
+// import { Box, FormControl, TextField, Typography } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
+import CardItem from "./CardItem/CardItem";
 
 function Shop() {
-  const classes = styles();
-
+  const stackStyle = {
+    paddingBottom: "250px",
+    paddingTop: "96px",
+  };
   return (
-    <Box>
-      <Typography
-        variant="h6"
-        sx={{ fontSize: "33px" }}
-        style={classes.formFilterContainer}
+    <Stack
+      spacing={{ xs: 1, sm: 4 }}
+      direction="row"
+      useFlexGap
+      flexWrap="wrap"
+      style={stackStyle}
+      // alignItems="center"
+    >
+      <div>side</div>
+      <Grid
+        container
+        columnSpacing={{ xs: 2, md: 3 }}
+        rowSpacing={{ xs: 3, md: 9 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        Shop The Latest
-      </Typography>
-      <FormControl>
-        <TextField
-          name="search"
-          type="text"
-          placeholder="Search"
-          sx={{ height: "35px" }}
-        />
-      </FormControl>
-    </Box>
+        {Array.from(Array(12)).map((_, index) => (
+          <Grid item xs={12} sm={4} md={4} key={index}>
+            <CardItem />
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
   );
 }
 export default Shop;
