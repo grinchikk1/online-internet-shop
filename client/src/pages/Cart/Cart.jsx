fa5403f353e42ba51b57da72658e5f2878df05d4
ArthurCherychenko <cherrych27@gmail.com>
2023-07-31 21:58:05 +0300

    fixed styles add MUI

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -5,6 +5,23 @@ import { PRODUCTS } from "../../product";
 import { ShopContext } from "../../components/context/shop-context";
 import CartItem from "./CartItem";
 
+import { theme } from "../Contact/ContactStyles";
+import {
+  Accordion,
+  AccordionSummary,
+  Autocomplete,
+  Container,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import { ThemeProvider } from "@mui/material/styles";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Button from "@mui/material/Button";
+
 const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext);
   const totalAmount = getTotalCartAmount();
@@ -23,20 +40,118 @@ const Cart = () => {
               }
             })}
           </div>
+          {totalAmount === 0 && (
+            <h1 className={s.cart_empty}>Your cart is EMPTY</h1>
+          )}
         </div>
-        <div className={s.frame_wrapper}>
-          <div className={s.frame_title}>Cart totals</div>
-        </div>
+        <ThemeProvider theme={theme}>
+          <Container maxWidth="lg">
+            <Grid item xs={12} md={6}>
+              <Typography variant="h5" gutterBottom>
+                Cart totals
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <div className={s.card_subtotal_wrapper}>
+                    <div className={s.cart_subtotal}>
+                      <h4>SUBTOTAL</h4>
+                      <h4>SHIPPING</h4>
+                    </div>
+                    <Grid item xs={12}>
+                      <div className={s.cart_amount}>
+                        <div>$ {totalAmount}</div>
+                        <p>
+                          Shipping costs will be calculated once you have
+                          provided address.
+                        </p>
+                        <Accordion variant="standard">
+                          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>CALCULATE SHIPPING</Typography>
+                          </AccordionSummary>
+                          <Stack spacing={1}>
+                            <Autocomplete
+                              id="SELECT A COUNTRY"
+                              disableCloseOnSelect
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  label="SELECT A COUNTRY"
+                                  variant="standard"
+                                />
+                              )}
+                            />
+                            <Autocomplete
+                              id="CITY"
+                              clearOnEscape
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  label="CITY"
+                                  variant="standard"
+                                />
+                              )}
+                            />
+                            <Autocomplete
+                              id="POST CODE / ZIP"
+                              disableClearable
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  label="POST CODE / ZIP"
+                                  variant="standard"
+                                />
+                              )}
+                            />
+                          </Stack>
+                        </Accordion>
+                        <Button
+                          color="inherit"
+                          style={{
+                            marginTop: "24px",
+                            height: "53px",
+                            fontWeight: "700",
+                          }}
+                          variant="outlined"
+                        >
+                          UPDATE TOTALS
+                        </Button>
+                      </div>
+                    </Grid>
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  <div
+                    style={{
+                      borderBottom: "1px solid #D8D8D8",
+                      paddingBottom: "24px",
+                    }}
+                  ></div>
+                </Grid>
+                <Grid item xs={12}>
+                  <div className={s.cart_total}>
+                    <div>TOTAL</div>
+                    <div>$ {totalAmount}</div>
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="outlined"
+                    style={{
+                      width: "100%",
+                      height: "53px",
+                      background: "black",
+                      color: "white",
+                    }}
+                    type="submit"
+                  >
+                    PROCEED TO CHECKOUT
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Container>
+        </ThemeProvider>
       </div>
-      {totalAmount > 0 ? (
-        <div className={s.cart_checkout}>
-          <p>Subtotal: {totalAmount}$</p>
-          <button onClick={() => navigate("/shop")}>Continue shopping</button>
-          <button>Checkout</button>
-        </div>
-      ) : (
-        <h1>Your cart is EMPTY</h1>
-      )}
     </>
   );
 };
--- client/src/pages/Cart/CartItem.jsx
+++ client/src/pages/Cart/CartItem.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import s from "./cartItem.module.css";
 import { ShopContext } from "../../components/context/shop-context";
 
@@ -17,33 +17,65 @@ const CartItem = (props) => {
     date,
     country,
   } = props.data;
+  const SVGCLOSEBUTTON = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+    >
+      <path
+        d="M1 1.18344L12.8166 13M1 12.8166L12.8166 1"
+        stroke="black"
+        strokeWidth="1.5"
+      />
+    </svg>
+  );
   const { cartItems, addToCart, removeFromCart, updateCartCount } =
     useContext(ShopContext);
 
+  const [isCardOpen, setIsCardOpen] = useState(true);
+
+  const handleCloseCard = () => {
+    setIsCardOpen(false);
+  };
+
   return (
-    <div className={s.item_wrapper}>
-      <img src={image} alt="#" className={s.item_image} />
-      <div className={s.wrapp_description}>
-        <div className={s.item_name}>{name}</div>
-        <p className={s.item_description}>
-          {productMaterial} / {brand}
-        </p>
-        <p className={s.item_price}>{currentPrice} $</p>
-      </div>
-      <div className={s.count_wrapper}>
-        <button className={s.count_button} onClick={() => removeFromCart(id)}>
-          -
-        </button>
-        <input
-          className={s.count_input}
-          value={cartItems[id]}
-          onChange={(e) => updateCartCount(Number(e.target.value), id)}
-        />
-        <button className={s.count_button} onClick={() => addToCart(id)}>
-          +
-        </button>
-      </div>
-    </div>
+    <>
+      {isCardOpen && (
+        <div className={s.item_wrapper}>
+          <img src={image} alt="#" className={s.item_image} />
+          <div className={s.wrapp_description}>
+            <div className={s.item_name}>{name}</div>
+            <p className={s.item_description}>
+              {productMaterial} / {brand}
+            </p>
+            <p className={s.item_price}>{currentPrice} $</p>
+          </div>
+          <div className={s.count_wrapper}>
+            <button
+              className={s.count_button}
+              onClick={() => removeFromCart(id)}
+            >
+              -
+            </button>
+            <input
+              className={s.count_input}
+              value={cartItems[id]}
+              onChange={(e) => updateCartCount(Number(e.target.value), id)}
+            />
+            <button className={s.count_button} onClick={() => addToCart(id)}>
+              +
+            </button>
+          </div>
+          <button className={s.close_button} onClick={handleCloseCard}>
+            <div>{SVGCLOSEBUTTON}</div>
+          </button>
+        </div>
+      )}
+      {isCardOpen && <span className={s.cart_line} />}
+    </>
   );
 };
 
