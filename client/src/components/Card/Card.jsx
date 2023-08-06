a2d52d50143f8a08444f7267fa9a5400d1eb9f31
Andrii Melnyk <melnyk.ask@gmail.com>
2023-08-06 21:40:37 +0300

    home page fixed

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import { Container, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { useStyles, theme } from "./CardStyle";
+import {
+  cardContainer,
+  cardImgContainer,
+  cardNameContainer,
+  cardHover,
+  cardHoverAdd,
+  cardName,
+  cardPrice,
+  cardMaterial,
+  cardBrand
+} from "./CardStyle";
 import heart from "./img/heart.svg";
+
 
 function Card({
   id,
@@ -22,26 +34,57 @@ function Card({
 
   return (
     <ThemeProvider theme={theme}>
-      <Container className={styles.cardContainer}>
-        <Container className={styles.cardImgContainer}>
+      <Container
+        // className={styles.cardContainer}
+        className="cardContainer"
+        sx={cardContainer }
+      >
+        <Container
+          // className={styles.cardImgContainer}
+          sx={cardImgContainer }
+        >
           <img src={image} alt="product" className={styles.cardImg} />
-          <Container className={styles.cardHover}>
-            <Typography className={styles.cardHoverAdd}>ADD TO CART</Typography>
+          <Container 
+          // className={styles.cardHover}
+          className="cardHover"
+          sx={cardHover }
+          >
+            <Typography 
+            // className={styles.cardHoverAdd}
+            sx={cardHoverAdd }
+            >
+              ADD TO CART
+            </Typography>
             <img src={heart} alt="heart" className={styles.cardHoverImg} />
           </Container>
         </Container> 
-        < Container className={ styles.cardNameContainer}>
-          <Typography className={ styles.cardName}>
+        <Typography
+        //  className={ styles.cardName}
+         sx={ cardName}
+         >
           { name }
+        </Typography>
+        < Container
+          // className={styles.cardNameContainer}
+          sx={ cardNameContainer}
+        >
+          <Typography
+            // className={styles.cardPrice}
+            sx={ cardPrice}
+          >
+          $ { currentPrice },00
           </Typography>
-          <Typography className={ styles.cardMaterial}>
+          <Typography
+            // className={styles.cardMaterial}
+            sx={ cardMaterial}
+          >
             { productMaterial }
           </Typography>
-        </Container >        
-        <Typography className={ styles.cardPrice}>
-          $ { currentPrice },00
-        </Typography>  
-        <Typography className={ styles.cardBrand}>
+        </Container > 
+        <Typography
+          // className={styles.cardBrand}
+          sx={ cardBrand}      
+        >
           { brand }
         </Typography>
       </Container>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { Container, Box, Typography, Button} from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import { useStyles, theme } from "./HomeStyles";
+import { Container, Box, Typography, Button} from "@mui/material";
 import Card from "../../components/Card/Card";
 import Carousel from "react-material-ui-carousel";
+import { useStyles, theme } from "./HomeStyles";
 import { useEffect, useState } from "react";
 import getData from "../../data/index";
 // import CardItem from "../Shop/CardItem/CardItem";
 import { Link } from "react-router-dom";
-
-
+//=========================
+import {
+  container0,
+  container1,
+  container2,
+  container3,
+  container4,
+  carousel,
+  btn,
+  name,
+  price,
+  latest,
+  btnAll,
+  carouselIcon,
+  carouselItem1,
+  carouselItem2,
+  carouselItem3,
+  carouselItem4,
+  carouselItem5
+} from "./HomeStyles";
 
 function Home() {
   let [products, setProducts] = useState([]);
@@ -18,24 +36,17 @@ function Home() {
   const output = out();
 
   useEffect(() => { 
-    async function fetchJson() {
-      //===!!== Not correct file location. Have to relocate file to client\PUBLIC.
-      //===!!== But fetch request from PUBLIC doesn't work...
-      // let request = await fetch("../../data.json");
-      
-      let request = await getData();  
-      
-      // let response = await request.json();
-      // setProducts(response);
+    async function fetchJson() {      
+      let request = await getData(); 
       setProducts(request);
     }
     fetchJson();
   }, []);
 
-  function viewAllProducts() {
-    let temp = viewAll;
-    setViewAll(!temp);
-  }
+  // function viewAllProducts() {
+  //   let temp = viewAll;
+  //   setViewAll(!temp);
+  // }
 
   function out () { 
     if (products) { 
@@ -45,7 +56,7 @@ function Home() {
             if (viewAll === true) {
               return (
                 <Card
-                  className={styles.card}
+                  // className={styles.card}
                   key={card.id}
                   id={card.id}
                   enabled={card.enabled}
@@ -67,7 +78,7 @@ function Home() {
               if (card.id > products.length-6) {
                 return (
                   <Card
-                    className={styles.card}
+                    // className={styles.card}
                     key={card.id}
                     id={card.id}
                     enabled={card.enabled}
@@ -97,67 +108,145 @@ function Home() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container className={styles.container0}>
-        <Container className={styles.container1}>
-          <Container className={styles.container2}>
+      <Container 
+        // className={styles.container0}
+        sx={container0 }
+      >
+        <Container
+          // className={styles.container1}
+          sx={container1 }
+        >
+          <Container
+            // className={styles.container2}
+            sx={container2 }
+          >
             <Carousel
               autoPlay={false}
-              className={styles.carousel}
-              IndicatorIcon={<Box className={styles.carouselIcon} />}
+              // className={styles.carousel}
+              sx={carousel}
+              IndicatorIcon={<Box
+                // className={styles.carouselIcon}
+                sx={carouselIcon}
+              />}
               indicatorContainerProps={{
-                className: styles.carouselIconContainer,
-                // style: {
-                //   zIndex: "10",
-                //   position: "absolute",
-                //   top: "70%",
-                // },
+                //  className: styles.carouselIconContainer,
+                style: {
+                  zIndex: "10",
+                  position: "absolute",
+                  top: "70%",
+                },
               }}
               activeIndicatorIconButtonProps={{
-                className: styles.carouselActiveIcon,
-                // style: {
-                //   width: "20px",
-                //   height: "20px",
-                //   backgroundColor: "transparent",
-                //   color: "transparent",
-                //   border: "1px solid white",
-                // }
+                //  className: styles.carouselActiveIcon,
+                sx: {
+                  width: "18px",
+                  height: "18px",
+                  backgroundColor: "transparent",
+                  color: "transparent",
+                  border: "1px solid white",                  
+                    "@media (max-width: 768.9px)": {
+                    width: "10px",
+                    height: "10px",
+                  },
+                },
+              //  style: {
+                //  width: "20px",
+                //  height: "20px",
+                //  backgroundColor: "transparent",
+                //  color: "transparent",
+                //  border: "1px solid white",
+              //  }
               }}
               indicatorIconButtonProps={{
+              // className: styles.carouselIcon,
                 // style: {
                 //   margin: "8px",
                 //   width: "8px",
                 //   height: "8px",
                 //   color: "#FFFFFF",
                 //   backgroundColor: "#FFFFFF",
+                //   "@media (max-width: 768.9px)": {
+                //     width: "18px",
+                //   height: "18px",
+                //   },
                 // }
-              }}
-            >
-              <Box className={styles.carouselItem1} />
-              <Box className={styles.carouselItem2} />
-              <Box className={styles.carouselItem3} />
-              <Box className={styles.carouselItem4} />
-              <Box className={styles.carouselItem5} />
-            </Carousel>
-          </Container>        
-          <Button variant="outlined" className={styles.btn}>
-          View Product
+             }}
+           >
+              <Box
+                // className={styles.carouselItem1}
+                sx={carouselItem1 }
+              />
+              <Box
+                // className={styles.carouselItem2}
+                sx={carouselItem2 }
+              />
+              <Box
+                // className={styles.carouselItem3}
+                sx={carouselItem3 }
+              />
+              <Box
+                // className={styles.carouselItem4}
+                sx={carouselItem4 }
+              />
+              <Box
+                // className={styles.carouselItem5}
+                sx={carouselItem5 }
+              />
+           </Carousel>
+         </Container>        
+          <Button
+            variant="outlined"
+            // className={styles.btn}
+            sx={btn} >
+            View Product
           </Button>
-          <Typography className={ styles.name}>Gold big hoops</Typography>
-          <Typography className={ styles.price}>$ 68,00</Typography>
-        </Container>      
-        <Container className={styles.container3}>
-          {/* { viewAll && <Typography className={styles.latest}>Shop All</Typography>} */}
-          {/* { !viewAll && <Typography className={styles.latest}>Shop The Latest</Typography>}   */}
-          {/* { !viewAll && <Button variant="text" className={styles.btnAll} onClick={viewAllProducts}>View All</Button>} */}
-          {/* { viewAll && <Button variant="text" className={styles.btnAll} onClick={viewAllProducts}>View The Latest</Button>} */}
-          <Typography className={styles.latest}>Shop The Latest</Typography>
-          <Link className={styles.link} to="/Shop"><Button variant="text" className={styles.btnAll} onClick={viewAllProducts}>View All</Button></Link>
-        </Container>
-        <Container className={styles.container4}>
-          { output}
-        </Container>
-      </Container>      
-    </ThemeProvider>
+          <Typography
+            // className={styles.name}
+            sx={name}
+          >
+            Gold big hoops
+          </Typography>
+          <Typography
+            // className={styles.price}
+            sx={price}
+          >
+            $ 68,00
+          </Typography>
+       </Container>      
+        <Container
+          // className={styles.container3}
+          sx={container3}
+        >
+         {/* { viewAll && <Typography className={styles.latest}>Shop All</Typography>} */}
+         {/* { !viewAll && <Typography className={styles.latest}>Shop The Latest</Typography>}   */}
+         {/* { !viewAll && <Button variant="text" className={styles.btnAll} onClick={viewAllProducts}>View All</Button>} */}
+         {/* { viewAll && <Button variant="text" className={styles.btnAll} onClick={viewAllProducts}>View The Latest</Button>} */}
+          <Typography
+            // className={styles.latest}
+            sx={latest}
+          >
+            Shop The Latest
+          </Typography>
+          <Link className={styles.link} to="/Shop">
+            <Button
+              variant="text"
+              // className={styles.btnAll}
+              sx={btnAll }
+              // onClick={viewAllProducts}
+            >
+              View All
+            </Button>
+          </Link>
+       </Container>
+        <Container
+          // className={styles.container4}
+          sx={container4}
+        >
+         { output}
+       </Container>
+     </Container>      
+   </ThemeProvider>
   );
 }
 export default Home;
+
