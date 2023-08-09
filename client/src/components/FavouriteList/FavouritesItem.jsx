d56db86630abf504fc657d171f8e1b140042dc24
Vladyslav-Honcharov <tawzi14@gmail.com>
2023-08-09 18:49:32 +0300

    Add more logic for favorites

--- client/src/components/FavouriteList/FavouritesItem.jsx
+++ client/src/components/FavouriteList/FavouritesItem.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import HeartBrokenIcon from "@mui/icons-material/HeartBroken";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addFavorites,
   removeFavorites,
 } from "../../features/favorites/favoriteSlice";
+import { useNavigate } from "react-router-dom";
 
 function FavouriteItem({ item }) {
-  const { imageUrls, name, brand } = item;
-
+  const { imageUrls, name, brand, _id } = item;
   const dispatch = useDispatch();
   const favoritesList = useSelector((state) => state.favorites.favoritesList);
   const isFavorited = favoritesList.some((product) => product._id === item._id);
@@ -22,9 +22,25 @@ function FavouriteItem({ item }) {
     }
   };
 
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/product/${_id}`);
+  };
+
   return (
-    <Card sx={{ maxWidth: 300, marginBottom: "20px" }}>
-      <CardMedia component="img" height="140" image={imageUrls[0]} alt={name} />
+    <Card
+      sx={{ width: "50vw", maxWidth: "400px", marginBottom: "20px" }}
+      onClick={handleCardClick}
+    >
+      <div
+        style={{
+          backgroundImage: `url(${imageUrls[0]})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "200px",
+        }}
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {name}
@@ -35,7 +51,10 @@ function FavouriteItem({ item }) {
           </Typography>
           <HeartBrokenIcon
             sx={{ cursor: "pointer", ":hover": { color: "#D82700" } }}
-            onClick={handleToggleFavorite}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleToggleFavorite();
+            }}
           />
         </div>
       </CardContent>
