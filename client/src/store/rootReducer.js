a505a66be5572d86c3145fc1e0a42ba5e5b5f690
Dmytro Hrinchenko <106904640+grinchikk1@users.noreply.github.com>
2023-08-21 18:36:37 +0300

    Update rootReducer.js

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -5,7 +5,6 @@ import cartReducer from "../features/cart/cartSlice";
 import reviewReducer from "../features/review/reviewSlice"
 import customerReducer from "../features/customer/customerSlice";
 import authReducer from "../features/auth/authSlice";
-import reviewReducer from "../features/review/reviewSlice";
 
 const rootReducer = combineReducers({
   favorites: favoritesReducer,
