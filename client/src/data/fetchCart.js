c16de68812812c94c857c7124e41641280079d71
Taisiia Zhdanovych <taisiya.zhdan@gmail.com>
2023-08-21 16:36:49 +0300

    create post and delete reviews in poducts

--- client/src/data/fetchCart.js
+++ client/src/data/fetchCart.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:4000/api";
+ export const url = "http://localhost:4000/api";
 
 // Створити кошик
 export const createCart = async (cart, token) => {
--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import favoritesReducer from "../features/favorites/favoriteSlice";
 import shopReducer from "../features/shop/shopSlice";
 import cartReducer from "../features/cart/cartSlice";
+import reviewReducer from "../features/review/reviewSlice"
 
 const rootReducer = combineReducers({
   favorites: favoritesReducer,
   shop: shopReducer,
   cart: cartReducer,
+  reviews: reviewReducer,
 });
 
 export default rootReducer;
